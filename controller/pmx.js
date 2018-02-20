6d9e7929f9ec9b4d5105bb2097376a4c1b07b5fa
dxl <870188670@qq.com>
2018-02-20 23:12:17 +0800

    pmx最新指标

--- controller/pmx.js
+++ controller/pmx.js
@@ -1,21 +1,33 @@
 // add metric 获取更多指标
 exports.pmx=()=>{
-
-var probe = require('pmx').probe();
 var pmx = require('pmx').init({
   network       : true,  //选项将显示入站和出站流量
   ports         : true,  //将显示您的应用程序正在使用的端口
 });
-var counter = 0;
 
-var metric = probe.metric({
-  name    : 'Realtime user',
-  value   : function() {
-    return counter;
-  }
+var probe = pmx.probe();
+
+var meter = probe.meter({
+  name      : 'req/sec',
+  samples   : 1,
+  timeframe : 60
 });
 
+http.createServer(function(req, res) {
+  meter.mark();
+  res.end({success:true});
+});
+
+
+var histogram = probe.histogram({
+  name        : 'latency',
+  measurement : 'mean'
+});
+
+var latency = 0;
+
 setInterval(function() {
-  counter++;
+  latency = Math.round(Math.random() * 100);
+  histogram.update(latency);
 }, 100);
 }
