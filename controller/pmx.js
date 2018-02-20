f95665330829485271910fc67e69b7954323beee
dxl <870188670@qq.com>
2018-02-20 21:59:20 +0800

    pmx指标

--- controller/pmx.js
+++ controller/pmx.js
@@ -1,7 +1,11 @@
 // add metric 获取更多指标
 exports.pmx=()=>{
-var probe = require('pmx').probe();
 
+var probe = require('pmx').probe();
+var pmx = require('pmx').init({
+  network       : true,  //选项将显示入站和出站流量
+  ports         : true,  //将显示您的应用程序正在使用的端口
+});
 var counter = 0;
 
 var metric = probe.metric({
