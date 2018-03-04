d073cffe00e50bd0cb177fc24d9d108c58084ec9
dxlani <870188670@qq.com>
2018-03-04 19:33:17 +0800

    test

--- routes/getcsplog.js
+++ routes/getcsplog.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../dbpool');
 router.get('/',function(req,res,next){
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     var level="";
     switch(req.param('level'))
     {
