4921a6b2723cfb8281960144870ce0e528917037
dxlani <870188670@qq.com>
2018-03-06 09:27:26 +0800

    1

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(express.static('./demo'));
 //     next();
 // });
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '127.0.0.1');
+    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1');
     // res.header('Access-Control-Allow-Credentials', 'true');
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
